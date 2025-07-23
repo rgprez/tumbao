57a2366278d908b48f17ce3e3150c8980b1b2d0c
rgprez <rayprez@gmail.com>
2025-07-23 10:36:37 -0400

    Implement about section

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { SplitText } from "gsap/SplitText";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
+import About from "./components/About";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger, SplitText);
 
@@ -15,6 +16,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
     </main>
   );
 };
